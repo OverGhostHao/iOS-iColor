d03a852f889fa9672922a7b2361397aa663a0076
BigBilly <BigBilly8866@gmail.com>
2015-11-15 22:39:47 -0600

    draw

--- iColor/DrawVC.m
+++ iColor/DrawVC.m
@@ -7,10 +7,11 @@
 //
 
 #import "DrawVC.h"
+#import "SWRevealViewController.h"
 
 @interface DrawVC ()
-@property (weak, nonatomic) IBOutlet UIImageView *tempDrawImage;
-@property (weak, nonatomic) IBOutlet UIImageView *mainImage;
+@property UIImageView *tempDrawImage;
+@property UIImageView *mainImage;
 
 
 @end
@@ -18,35 +19,44 @@
 @implementation DrawVC
 
 - (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
     
+    self.tempDrawImage = [[UIImageView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
+    self.mainImage = [[UIImageView alloc]initWithFrame:CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y, self.view.frame.size.width, self.view.frame.size.height)];
+    [self.view addSubview:self.tempDrawImage];
+    [self.view addSubview:self.mainImage];
+    
+    //Set the list page for navigation
+    SWRevealViewController *revealViewController = self.revealViewController;
+    if (revealViewController) {
+        [self.sidebarButton setTarget:self.revealViewController];
+        [self.sidebarButton setAction:@selector(revealToggle:)];
+        [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    }
+    
+    self.title = @"Draw For You";
+    self.navigationController.navigationBar.barTintColor = [UIColor darkGrayColor];
+    self.navigationController.navigationBar.tintColor = [UIColor yellowColor];
+    [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:1 green:1 blue:0.447 alpha:1.0], NSForegroundColorAttributeName, [UIFont fontWithName:@"Arial Rounded MT Bold" size:20], NSFontAttributeName, nil]];
+    
+    //draw init
     red = 0.0/255.0;
     green = 0.0/255.0;
     blue = 0.0/255.0;
     brush = 10.0;
     opacity = 1.0;
-    [super viewDidLoad];
     
-    // Do any additional setup after loading the view from its nib.
-    UILabel *drawLabel = [[UILabel alloc]initWithFrame:CGRectMake(100, 200, 100, 200)];
-    [drawLabel setText:@"Drawing"];
-    [self.view addSubview:drawLabel];
-
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    
     mouseSwiped = NO;
     UITouch *touch = [touches anyObject];
     lastPoint = [touch locationInView:self.view];
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    
     mouseSwiped = YES;
     UITouch *touch = [touches anyObject];
     CGPoint currentPoint = [touch locationInView:self.view];
@@ -69,7 +79,6 @@
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    
     if(!mouseSwiped) {
         UIGraphicsBeginImageContext(self.view.frame.size);
         [self.tempDrawImage.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
@@ -94,15 +103,5 @@
 
 
 
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
