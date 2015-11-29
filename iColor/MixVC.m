c8936fa1e819083fec60cc01e25bd56f839c581b
kennyhao <kennyhaoyuhan@gmail.com>
2015-11-28 23:01:29 -0600

    MixVC updated.
    ColorItem support global setting.

--- iColor/MixVC.m
+++ iColor/MixVC.m
@@ -9,6 +9,7 @@
 #import "MixVC.h"
 #import "SWRevealViewController.h"
 #import "ColorItem.h"
+#import <QuartzCore/QuartzCore.h>
 
 @interface MixVC ()
 
@@ -25,22 +26,8 @@
 //    [mixLabel setText:@"Mixing"];
 //    [self.view addSubview:mixLabel];
     
-    self.data = [NSArray arrayWithObjects:@"color1",@"color2",@"color3",@"color4",@"color5", nil];
     
-//    ColorItem *item1 = [[ColorItem alloc] init];
-//    [item1 setRGB:255 gValue:134 bValue:156];
-//    ColorItem *item2 = [[ColorItem alloc] init];
-//    [item1 setRGB:246 gValue:137 bValue:200];
-//    ColorItem *item3 = [[ColorItem alloc] init];
-//    [item1 setRGB:100 gValue:134 bValue:200];
-//    ColorItem *item4 = [[ColorItem alloc] init];
-//    [item1 setRGB:245 gValue:218 bValue:156];
-//    ColorItem *item5 = [[ColorItem alloc] init];
-//    [item1 setRGB:100 gValue:134 bValue:244];
-//    
-//    self.color = [NSMutableArray arrayWithObjects:item1,item2,item3,item4,item5, nil];
-    
-    self.color = [NSMutableArray arrayWithObjects:[UIColor redColor],[UIColor greenColor],[UIColor yellowColor],[UIColor blueColor],[UIColor purpleColor], nil];
+    //self.color = [NSMutableArray arrayWithObjects:[UIColor redColor],[UIColor greenColor],[UIColor yellowColor],[UIColor blueColor],[UIColor purpleColor], nil];
     
     //Set the list page for navigation
     SWRevealViewController *revealViewController = self.revealViewController;
@@ -55,6 +42,12 @@
     self.navigationController.navigationBar.tintColor = [UIColor yellowColor];
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor colorWithRed:1 green:1 blue:0.447 alpha:1.0], NSForegroundColorAttributeName, [UIFont fontWithName:@"Arial Rounded MT Bold" size:20], NSFontAttributeName, nil]];
     
+    numLine = 5;
+    
+//    CGFloat x = self.view.frame.origin.x;
+//    CGFloat y = self.view.frame.origin.y+self.navigationController.navigationBar.layer.frame.size.height+100;
+//    CGFloat width = self.view.frame.size.width;
+//    CGFloat height = (self.view.frame.size.height-100)/5;
     
     self.tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen]applicationFrame] style:UITableViewStyleGrouped];
     self.tableView.delegate = self;
@@ -65,74 +58,145 @@
     [self.tableView setBackgroundColor:[UIColor grayColor]];
     [self.tableView setScrollEnabled:NO];
     
-    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIButton *button1 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button1.layer.cornerRadius = 15;
+    button1.clipsToBounds = YES;
     [button1 addTarget:self action:@selector(button1Action:) forControlEvents:UIControlEventTouchUpInside];
     [button1 setTitle:@"COLOR1" forState:UIControlStateNormal];
-    [button1 setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
-    button1.frame = CGRectMake(10, -15, 100, 40);
-    button1.backgroundColor = [UIColor purpleColor];
+    [button1 setTitleColor:[UIColor colorWithRed:1 green:1 blue:0.447 alpha:1.0] forState:UIControlStateNormal];
+    button1.frame = CGRectMake(10, -13, 100, 40);
+    button1.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:18];
+    button1.backgroundColor = [UIColor darkGrayColor];
     [self.tableView addSubview:button1];
     
-    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+    UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    button2.layer.cornerRadius = 15;
+    button2.clipsToBounds = YES;
     [button2 addTarget:self action:@selector(button2Action:) forControlEvents:UIControlEventTouchUpInside];
     [button2 setTitle:@"COLOR2" forState:UIControlStateNormal];
-    [button2 setTitleColor:[UIColor yellowColor] forState:UIControlStateNormal];
-    button2.frame = CGRectMake(120, -15, 100, 40);
-    button2.backgroundColor = [UIColor purpleColor];
+    [button2 setTitleColor:[UIColor colorWithRed:1 green:1 blue:0.447 alpha:1.0] forState:UIControlStateNormal];
+    button2.frame = CGRectMake(120, -13, 100, 40);
+    button2.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:18];
+    button2.backgroundColor = [UIColor darkGrayColor];
     [self.tableView addSubview:button2];
+    
+    //Create the slider
+    NSArray *itemArray = [NSArray arrayWithObjects:@"5",@"6",@"7",@"8",@"9",@"10", nil];
+    UISegmentedControl *levelSegment = [[UISegmentedControl alloc] initWithItems:itemArray];
+    levelSegment.frame = CGRectMake(230, -13, 180, 40);
+    [levelSegment addTarget:self action:@selector(changeLevel:) forControlEvents:UIControlEventValueChanged];
+    [levelSegment setTintColor:[UIColor colorWithRed:1 green:1 blue:0.447 alpha:1.0]];
+    levelSegment.selectedSegmentIndex = 0;
+    [levelSegment setBackgroundColor:[UIColor clearColor]];
+    [self.tableView addSubview:levelSegment];
+    
+    //init random colors;
+    self.color = [[NSMutableArray alloc]init];
+    for (int i = 0; i < 5; i++) {
+        ColorItem *newC = [[ColorItem alloc]init];
+        newC = [self generateRandomColor];
+        [self.color addObject:newC];
+    }
+    
+}
+
+-(ColorItem *) generateRandomColor {
+    NSInteger red = arc4random_uniform(256);
+    NSInteger green = arc4random_uniform(256);
+    NSInteger blue = arc4random_uniform(256);
+    ColorItem *newColor = [[ColorItem alloc]init];
+    [newColor setRGB:red gValue:green bValue:blue];
+    return newColor;
+    
+}
+
+-(void)changeLevel: (UISegmentedControl *)sender{
+    numLine = 5+sender.selectedSegmentIndex;
+    for (int i=5; i<numLine; i++) {
+        ColorItem *newC = [[ColorItem alloc] init];
+        newC = [self generateRandomColor];
+        [self.color addObject:newC];
+    }
+    [self.tableView reloadData];
     
 }
 
 -(void)button1Action: (id)sender{
-    ColorItem *testColor = [[ColorItem alloc]init];
-    [testColor setRGB:102 gValue:0 bValue:204];
+//    ColorItem *testColor = [[ColorItem alloc]init];
+//    [testColor setRGB:102 gValue:0 bValue:204];
+//    
+//    [self.color replaceObjectAtIndex:0 withObject:testColor];
+//    [self.tableView reloadData];
     
-    [self.color replaceObjectAtIndex:0 withObject:testColor.myUIColor];
+    ColorItem *testColor = [[ColorItem alloc]init];
+    testColor = [self generateRandomColor];
+    [self.color replaceObjectAtIndex:0 withObject:testColor];
     [self.tableView reloadData];
 }
 
 -(void)button2Action: (id)sender{
-    ColorItem *testColor5 = [[ColorItem alloc]init];
-    [testColor5 setRGB:204 gValue:153 bValue:255];
     
-//    ColorItem *testColor1 = [self.color objectAtIndex:0];
-    
-    
-//    ColorItem *testColor2 = [[ColorItem alloc] init];
-    
-
-//    [testColor2 setRGB:(MIN(testColor1.rValue,testColor5.rValue)+ABS(testColor1.rValue-testColor5.rValue))*2/5 gValue:100 bValue:200];
-//    CGFloat r2 = (MIN(testColor1.rValue,testColor5.rValue)+ABS(testColor1.rValue-testColor5.rValue))*2/5;
-//    CGFloat g2 = (MIN(testColor1.gValue,testColor5.gValue)+ABS(testColor1.gValue-testColor5.gValue))*2/5;
-//    CGFloat b2 = (MIN(testColor1.bValue,testColor5.bValue)+ABS(testColor1.bValue-testColor5.bValue))*2/5;
-
-//    [testColor2 setRGB:122 gValue:133 bValue:144];
-    
-    ColorItem *testColor1 = [[ColorItem alloc]init];
-    [testColor1 setRGB:127 gValue:0 bValue:255];
-    
-    ColorItem *testColor2 = [[ColorItem alloc]init];
-    [testColor2 setRGB:153 gValue:51 bValue:255];
-    
-    ColorItem *testColor3 = [[ColorItem alloc]init];
-    [testColor3 setRGB:178 gValue:102 bValue:255];
+    ColorItem *testColorLast = [[ColorItem alloc]init];
+    testColorLast = [self generateRandomColor];
+    [self.color replaceObjectAtIndex:(numLine-1) withObject:testColorLast];
 
     
-    [self.color replaceObjectAtIndex:1 withObject:testColor1.myUIColor];
-    [self.color replaceObjectAtIndex:2 withObject:testColor2.myUIColor];
-    [self.color replaceObjectAtIndex:3 withObject:testColor3.myUIColor];
-    [self.color replaceObjectAtIndex:4 withObject:testColor5.myUIColor];
+    
+    for (int i=1; i<=(numLine-2); i++) {
+        ColorItem *colorNow = [[ColorItem alloc] init];
+        colorNow = [self mixColor:i andLastColor:testColorLast];
+        [self.color replaceObjectAtIndex:i withObject:colorNow];
+    }
+    
     [self.tableView reloadData];
+    
+}
+
+
+-(ColorItem *)mixColor:(int) index andLastColor:(ColorItem *)lastColor{
+    ColorItem *firstColor = [self.color objectAtIndex:0];
+    ColorItem *colorMix = [[ColorItem alloc] init];
+    
+    NSInteger rlow = MIN(firstColor.rValue, lastColor.rValue);
+    NSInteger rhigh = MAX(firstColor.rValue, lastColor.rValue);
+    NSInteger rmix;
+    if (firstColor.rValue == rlow) {
+       rmix = rlow + (float)(index-1)/(numLine) * (rhigh-rlow);
+    }else{
+        rmix = rlow - (float)(index-1)/(numLine) * (rhigh-rlow);
+    }
+    
+    NSInteger glow = MIN(firstColor.gValue, lastColor.gValue);
+    NSInteger ghigh = MAX(firstColor.gValue, lastColor.gValue);
+    NSInteger gmix;
+    if (firstColor.gValue == glow) {
+        gmix = glow + (float)(index-1)/(numLine) * (ghigh-glow);
+    }else{
+        gmix = glow - (float)(index-1)/(numLine) * (ghigh-glow);
+    }
+    
+    NSInteger blow = MIN(firstColor.bValue, lastColor.bValue);
+    NSInteger bhigh = MAX(firstColor.bValue, lastColor.bValue);
+    NSInteger bmix;
+    if (firstColor.bValue == blow) {
+        bmix = blow + (float)(index-1)/(numLine) * (bhigh-blow);
+    }else{
+        bmix = blow - (float)(index-1)/(numLine) * (bhigh-blow);
+    }
+    
+    
+    [colorMix setRGB:rmix gValue:gmix bValue:bmix];
+    return colorMix;
 }
 
 
 
 -(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return self.data ?1:0;
+    return self.color ?1:0;
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.data?self.data.count :0;
+    return self.color?self.color.count :0;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
@@ -140,14 +204,11 @@
     if (cell==nil) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"MyIdentifier"];
     }
-
-    NSString *myString;
-    myString = [self.data objectAtIndex:indexPath.row];
-    cell.textLabel.text = myString;
     
-    UIColor *thisColor;
+    ColorItem *thisColor;
     thisColor = [self.color objectAtIndex:indexPath.row];
-    cell.backgroundColor = thisColor;
+    cell.backgroundColor = thisColor.myUIColor;
+    cell.textLabel.text = thisColor.hexString;
     
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
@@ -155,7 +216,8 @@
 }
 
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 100;
+//    return self.view.frame.size.height/(numLine+1);
+    return (self.tableView.frame.size.height-98)/numLine;
 }
 
 
