83eb7a945ea6a6a0098f0fb9105a8a1eafc998b3
kennyhao <kennyhaoyuhan@gmail.com>
2015-11-30 15:29:44 -0600

    my colors update

--- iColor/CollectionViewCell.m
+++ iColor/CollectionViewCell.m
@@ -19,11 +19,11 @@
     [c setRGB:255 gValue:100 bValue:20];
     self.backgroundView.backgroundColor = c.myUIColor;
     
-    UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
-    self.selectedBackgroundView = selectedView;
-    ColorItem *s = [[ColorItem alloc]init];
-    [s setRGB:255 gValue:255 bValue:255];
-    self.selectedBackgroundView.backgroundColor = s.myUIColor;
+//    UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
+//    self.selectedBackgroundView = selectedView;
+//    ColorItem *s = [[ColorItem alloc]init];
+//    [s setRGB:255 gValue:255 bValue:255];
+//    self.selectedBackgroundView.backgroundColor = s.myUIColor;
     
     [self.textLabel setFont:[UIFont fontWithName:@"American Typewriter" size:20]];
     
